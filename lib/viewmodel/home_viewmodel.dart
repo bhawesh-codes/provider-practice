2ba21b7649c81560fb7aab61f5461310e054a76b
Bhawesh <bhaweshbaruwal3@gmail.com>
2026-03-27 09:50:46 +0545

    feat: Add logic for Homeview update

--- lib/viewmodel/home_viewmodel.dart
+++ lib/viewmodel/home_viewmodel.dart
@@ -1,13 +1,13 @@
 import 'package:flutter/material.dart';
 
 class HomeViewmodel extends ChangeNotifier{
-  Color _color = Colors.red;
+  Color _color = Colors.redAccent;
   Color get color => _color;
   int _count = 0;
   String get count => _count.toString();
 
   void changeColor(){
-    _color == Colors.red ? _color= Colors.blueAccent : _color = Colors.red;
+    _color == Colors.redAccent ? _color= Colors.blueAccent : _color = Colors.redAccent;
     notifyListeners();
   }
   void changeCount(){
